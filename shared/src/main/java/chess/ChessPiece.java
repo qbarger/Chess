8af1a88e9e6bced7a226dc8f5e067085c65339ac
Quenton Barger <qbarger22@gmail.com>
2024-02-06 17:45:26 -0700

    making setPieceType to handle pawn Promotion.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -51,6 +51,10 @@ public class ChessPiece {
         return type;
     }
 
+    public void setPieceType(PieceType promotionPiece){
+        type = promotionPiece;
+    }
+
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
