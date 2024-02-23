385875266d7b9b0b10fc26daed4741c86a986737
Quenton Barger <qbarger22@gmail.com>
2024-02-22 18:28:38 -0700

    adding ClearServiceTest

---
+++ server/src/test/java/service/ClearServiceTest.java
@@ -0,0 +1,21 @@
+package service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClearServiceTest {
+
+
+
+  @BeforeEach
+  void setup(){
+
+  }
+
+  @Test
+  void clear() {
+
+  }
+}
