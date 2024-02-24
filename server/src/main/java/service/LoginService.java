cfc12abceba054d3d584ca88990dcd8bc4e13c3d
Quenton Barger <qbarger22@gmail.com>
2024-02-23 17:41:03 -0700

    Passed login service tests

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,11 +1,29 @@
 package service;
 
 import dataAccess.AuthDao;
+import dataAccess.DataAccessException;
 import dataAccess.UserDao;
+import model.AuthData;
+import model.UserData;
 import org.eclipse.jetty.server.Authentication;
 
 public class LoginService {
   private AuthDao authDB;
   private UserDao userDB;
 
+  public LoginService(UserDao userDB, AuthDao authDB){
+    this.userDB = userDB;
+    this.authDB = authDB;
+  }
+
+  public String login(UserData user) throws DataAccessException {
+    if(userDB.checkUser(user.username())){
+      authDB.createAuth(user.username());
+      AuthData auth = authDB.getAuth(user.username());
+      return auth.authToken();
+    }
+    else {
+      throw new DataAccessException("Username does not exist.");
+    }
+  }
 }
---
+++ server/src/test/java/service/LoginServiceTest.java
@@ -0,0 +1,45 @@
+package service;
+
+import dataAccess.*;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginServiceTest {
+  public RegisterService testObject0;
+
+  public LoginService testObject1;
+  public UserDao userTestDB;
+  public AuthDao authTestDB;
+
+  @BeforeEach
+  void setup(){
+    userTestDB = new MemoryUserDao();
+    authTestDB = new MemoryAuthDao();
+    testObject1 = new LoginService(userTestDB, authTestDB);
+    testObject0 = new RegisterService(userTestDB, authTestDB);
+  }
+  @Test
+  void login() throws DataAccessException {
+    String auth0 =testObject0.register(new UserData("Mikal", "bridges", "traded@gmail.com"));
+    String auth1 =testObject1.login(new UserData("Mikal", "bridges", "traded@gmail.com"));
+    AuthData testAuth = authTestDB.getAuth("Mikal");
+
+    assertEquals(auth1, testAuth.authToken());
+  }
+
+  @Test
+  void loginFails() throws DataAccessException {
+    try {
+      String auth1 =testObject1.login(new UserData("Mikal", "bridges", "traded@gmail.com"));
+
+      fail("Expected a Data Class Exception to be thrown.");
+    }
+    catch (DataAccessException d){
+      assertEquals("Username does not exist.", d.getMessage());
+    }
+  }
+}
